55a66abd291554d6b2a814a64b577d8df9c8276b
Stephan Schmiedmayer <56692976+StephanSchmiedmayer@users.noreply.github.com>
2024-03-12 14:53:09 +0100

    Move Domain separation tags to bbs.h

--- test/test_util.h
+++ test/test_util.h
@@ -8,24 +8,6 @@
 #include <time.h>
 #include <inttypes.h>
 
-// Magic constants to be used as Domain Separation Tags
-#if BBS_CIPHER_SUITE == BBS_CIPHER_SUITE_BLS12_381_SHA_256
-#define BBS_CIPHER_SUITE_ID "BBS_BLS12381G1_XMD:SHA-256_SSWU_RO_"
-#define BBS_CIPHER_SUITE_LENGTH 35
-#elif BBS_CIPHER_SUITE == BBS_CIPHER_SUITE_BLS12_381_SHAKE_256
-#define BBS_CIPHER_SUITE_ID "BBS_BLS12381G1_XOF:SHAKE-256_SSWU_RO_"
-#define BBS_CIPHER_SUITE_LENGTH 37
-#endif
-
-#define BBS_DEFAULT_KEY_DST BBS_CIPHER_SUITE_ID "KEYGEN_DST_"
-#define BBS_API_ID          BBS_CIPHER_SUITE_ID "H2G_HM2S_"
-#define BBS_API_ID_LENGTH   BBS_CIPHER_SUITE_LENGTH + 9
-#define BBS_SIGNATURE_DST   BBS_API_ID "H2S_"
-#define BBS_CHALLENGE_DST   BBS_API_ID "H2S_"
-#define BBS_MAP_DST         BBS_API_ID "MAP_MSG_TO_SCALAR_AS_HASH_"
-#define BBS_MAP_DST_LENGTH  BBS_API_ID_LENGTH + 26
-
-
 #define ASSERT_EQ(purpose, actual, ref) \
 	for(int i=0; i < sizeof(ref); i++) { \
 		if(actual[i] != ref[i]) { \
