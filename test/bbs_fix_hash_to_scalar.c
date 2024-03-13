428eb8a11de950143f06c8bec220da982ad483e7
Stephan Schmiedmayer <56692976+StephanSchmiedmayer@users.noreply.github.com>
2024-03-13 15:49:30 +0100

    Fix hash_to_scalar SHAKE test

--- test/bbs_fix_hash_to_scalar.c
+++ test/bbs_fix_hash_to_scalar.c
@@ -2,17 +2,19 @@
 #include "test_util.h"
 
 #if BBS_CIPHER_SUITE == BBS_CIPHER_SUITE_BLS12_381_SHA_256
-#define msg             fixture_bls12_381_sha_256_h2s_msg;
-#define dst             fixture_bls12_381_sha_256_h2s_dst;
-#define dst_len         48;
-#define expected_scalar fixture_bls12_381_sha_256_h2s_scalar;
-#define scalar_len      32;
+#define fixture_msg             fixture_bls12_381_sha_256_h2s_msg
+#define fixture_msg_len         32
+#define fixture_dst             fixture_bls12_381_sha_256_h2s_dst
+#define fixture_dst_len         48
+#define fixture_expected_scalar fixture_bls12_381_sha_256_h2s_scalar
+#define fixture_scalar_len      32
 #elif BBS_CIPHER_SUITE == BBS_CIPHER_SUITE_BLS12_381_SHAKE_256
-#define msg             fixture_bls12_381_shake_256_h2s_msg fixture_m_1
-#define dst             fixture_bls12_381_shake_256_h2s_dst;
-#define dst_len         48;
-#define expected_scalar fixture_bls12_381_shake_256_h2s_scalar;
-#define scalar_len      32;
+#define fixture_msg             fixture_bls12_381_shake_256_h2s_msg
+#define fixture_msg_len         32
+#define fixture_dst             fixture_bls12_381_shake_256_h2s_dst
+#define fixture_dst_len         50
+#define fixture_expected_scalar fixture_bls12_381_shake_256_h2s_scalar
+#define fixture_scalar_len      32
 #endif
 
 int
@@ -29,39 +31,28 @@ bbs_fix_hash_to_scalar ()
 		return 1;
 	}
 
-	bn_t out;
-	bn_null (out);
+	uint8_t bin[BBS_SCALAR_LEN];
+	bn_t    scalar;
+	bn_null (scalar);
 	RLC_TRY {
-		bn_new (out);
+		bn_new (scalar);
 	}
 	RLC_CATCH_ANY {
 		puts ("Internal Error");
 		return 1;
 	}
 
-	if (BBS_OK != hash_to_scalar (out, fixture_bls12_381_sha_256_h2s_dst, 48,
-				      fixture_bls12_381_sha_256_h2s_msg, 32, 0))
+	if (BBS_OK != hash_to_scalar (scalar, fixture_dst, fixture_dst_len, fixture_msg,
+				      fixture_msg_len, 0))
 	{
 		puts ("Error during hash to scalar");
 		return 1;
 	}
+	RLC_TRY {
+		bn_write_bbs (bin, scalar);
+	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
 
-	// Determine the length of the bn in bytes
-	int bn_len = bn_size_bin (out);
-
-	// Dynamically allocate a temporary buffer for the byte array
-	uint8_t*buffer = (uint8_t*) malloc (bn_len * sizeof(uint8_t));
-	if (buffer == NULL)
-	{
-		// Handle memory allocation failure
-		printf ("Memory allocation failed.\n");
-		return 1;
-	}
-
-	// Convert bn_t to byte array (big-endian)
-	bn_write_bin (buffer, bn_len, out);
-
-	ASSERT_EQ ("hash to scalar", buffer, fixture_bls12_381_sha_256_h2s_scalar);
+	ASSERT_EQ ("hash to scalar", bin, fixture_expected_scalar);
 
 	return 0;
 }
