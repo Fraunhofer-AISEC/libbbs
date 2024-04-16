9a6a4b11682304cff3af5208981952fdeb41b3d6
Stephan Schmiedmayer <56692976+StephanSchmiedmayer@users.noreply.github.com>
2024-04-16 14:02:19 +0200

    Mrege msg_scalar sha & shake tests

--- test/bbs_fix_msg_scalars.c
+++ test/bbs_fix_msg_scalars.c
@@ -1,217 +1,103 @@
 #include "fixtures.h"
 #include "test_util.h"
 
-#if BBS_CIPHER_SUITE == BBS_CIPHER_SUITE_BLS12_381_SHA_256
-#define cipher_suite          bbs_sha256_cipher_suite
-#define fixture_msg_scalar_1  fixture_bls12_381_sha_256_msg_scalar_1
-#define fixture_msg_scalar_2  fixture_bls12_381_sha_256_msg_scalar_2
-#define fixture_msg_scalar_3  fixture_bls12_381_sha_256_msg_scalar_3
-#define fixture_msg_scalar_4  fixture_bls12_381_sha_256_msg_scalar_4
-#define fixture_msg_scalar_5  fixture_bls12_381_sha_256_msg_scalar_5
-#define fixture_msg_scalar_6  fixture_bls12_381_sha_256_msg_scalar_6
-#define fixture_msg_scalar_7  fixture_bls12_381_sha_256_msg_scalar_7
-#define fixture_msg_scalar_8  fixture_bls12_381_sha_256_msg_scalar_8
-#define fixture_msg_scalar_9  fixture_bls12_381_sha_256_msg_scalar_9
-#define fixture_msg_scalar_10 fixture_bls12_381_sha_256_msg_scalar_10
-#elif BBS_CIPHER_SUITE == BBS_CIPHER_SUITE_BLS12_381_SHAKE_256
-#define cipher_suite          bbs_shake256_cipher_suite
-#define fixture_msg_scalar_1  fixture_bls12_381_shake_256_msg_scalar_1
-#define fixture_msg_scalar_2  fixture_bls12_381_shake_256_msg_scalar_2
-#define fixture_msg_scalar_3  fixture_bls12_381_shake_256_msg_scalar_3
-#define fixture_msg_scalar_4  fixture_bls12_381_shake_256_msg_scalar_4
-#define fixture_msg_scalar_5  fixture_bls12_381_shake_256_msg_scalar_5
-#define fixture_msg_scalar_6  fixture_bls12_381_shake_256_msg_scalar_6
-#define fixture_msg_scalar_7  fixture_bls12_381_shake_256_msg_scalar_7
-#define fixture_msg_scalar_8  fixture_bls12_381_shake_256_msg_scalar_8
-#define fixture_msg_scalar_9  fixture_bls12_381_shake_256_msg_scalar_9
-#define fixture_msg_scalar_10 fixture_bls12_381_shake_256_msg_scalar_10
+typedef struct
+{
+	bbs_cipher_suite_t  cipher_suite;
+	const uint8_t      *msg[10];
+	size_t              msg_len;
+} fixture_msg_scalar;
 
-#endif
 int
 bbs_fix_msg_scalars ()
 {
-	if (core_init () != RLC_OK)
-	{
-		core_clean ();
-		return 1;
-	}
-	if (pc_param_set_any () != RLC_OK)
-	{
-		core_clean ();
-		return 1;
-	}
+	fixture_msg_scalar test_cases[] = {
+		{
+			.cipher_suite = bbs_sha256_cipher_suite, 
+			.msg          = {
+				fixture_bls12_381_sha_256_msg_scalar_1,
+				fixture_bls12_381_sha_256_msg_scalar_2,
+				fixture_bls12_381_sha_256_msg_scalar_3,
+				fixture_bls12_381_sha_256_msg_scalar_4,
+				fixture_bls12_381_sha_256_msg_scalar_5,
+				fixture_bls12_381_sha_256_msg_scalar_6,
+				fixture_bls12_381_sha_256_msg_scalar_7,
+				fixture_bls12_381_sha_256_msg_scalar_8,
+				fixture_bls12_381_sha_256_msg_scalar_9,
+				fixture_bls12_381_sha_256_msg_scalar_10,
+			}, 
+			.msg_len = sizeof(fixture_bls12_381_sha_256_msg_scalar_1)
+		},
+		{
+			.cipher_suite = bbs_shake256_cipher_suite, 
+			.msg          = {
+				fixture_bls12_381_shake_256_msg_scalar_1,
+				fixture_bls12_381_shake_256_msg_scalar_2,
+				fixture_bls12_381_shake_256_msg_scalar_3,
+				fixture_bls12_381_shake_256_msg_scalar_4,
+				fixture_bls12_381_shake_256_msg_scalar_5,
+				fixture_bls12_381_shake_256_msg_scalar_6,
+				fixture_bls12_381_shake_256_msg_scalar_7,
+				fixture_bls12_381_shake_256_msg_scalar_8,
+				fixture_bls12_381_shake_256_msg_scalar_9,
+				fixture_bls12_381_shake_256_msg_scalar_10,
+			}, 
+			.msg_len = sizeof(fixture_bls12_381_shake_256_msg_scalar_1)
+		}
+	};
 
-	uint8_t bin[BBS_SCALAR_LEN];
-	bn_t    scalar;
-	bn_null (scalar);
-	RLC_TRY {
-		bn_new (scalar); // Yes, this might leak. This is a test and thus
-		                 // short lived
-	}
-	RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
+	uint8_t           *fixture_ms[10] = {
+		fixture_m_1, fixture_m_2, fixture_m_3, fixture_m_4, fixture_m_5, fixture_m_6,
+		fixture_m_7, fixture_m_8, fixture_m_9, fixture_m_10
+	};
+	uint32_t           fixture_ms_len[10] = {
+		sizeof(fixture_m_1), sizeof(fixture_m_2), sizeof(fixture_m_3), sizeof(fixture_m_4),
+		sizeof(fixture_m_5), sizeof(fixture_m_6), sizeof(fixture_m_7), sizeof(fixture_m_8),
+		sizeof(fixture_m_9), sizeof(fixture_m_10)
+	};
 
-	const uint8_t *map_dst     = (uint8_t *) cipher_suite.map_dst;
-	const uint8_t  map_dst_len = cipher_suite.map_dst_len;
-
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_1,
-				      sizeof(fixture_m_1),
-				      0))
+	for (int cipher_suite_index = 0; cipher_suite_index < 2; cipher_suite_index++)
 	{
-		puts ("Error during hash to scalar for message 1");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 1 generation", bin, fixture_msg_scalar_1);
+		fixture_msg_scalar fixture = test_cases[cipher_suite_index];
+		if (core_init () != RLC_OK)
+		{
+			core_clean ();
+			return 1;
+		}
+		if (pc_param_set_any () != RLC_OK)
+		{
+			core_clean ();
+			return 1;
+		}
 
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_2,
-				      sizeof(fixture_m_2),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 2");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 2 generation", bin, fixture_msg_scalar_2);
+		uint8_t bin[BBS_SCALAR_LEN];
+		bn_t    scalar;
+		bn_null (scalar);
+		RLC_TRY {
+			bn_new (scalar); // Yes, this might leak. This is a test and thus
+			                 // short lived
+		}
+		RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
 
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_3,
-				      sizeof(fixture_m_3),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 3");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 3 generation", bin, fixture_msg_scalar_3);
+		const uint8_t *map_dst     = (uint8_t *) fixture.cipher_suite.map_dst;
+		const uint8_t  map_dst_len = fixture.cipher_suite.map_dst_len;
 
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_4,
-				      sizeof(fixture_m_4),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 4");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 4 generation", bin, fixture_msg_scalar_4);
+		for (int i = 0; i < 10; i++)
+		{
 
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_5,
-				      sizeof(fixture_m_5),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 5");
-		return 1;
+			if (BBS_OK != hash_to_scalar (&fixture.cipher_suite, scalar, map_dst,
+						      map_dst_len, fixture_ms[i], fixture_ms_len[i],
+						      0))
+			{
+				puts ("Error during hash to scalar for message 1");
+				return 1;
+			}
+			RLC_TRY {
+				bn_write_bbs (bin, scalar);
+			} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
+			ASSERT_EQ_PTR ("scalar 1 generation", bin, fixture.msg[i], fixture.msg_len);
+		}
 	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 5 generation", bin, fixture_msg_scalar_5);
-
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_6,
-				      sizeof(fixture_m_6),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 6");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 6 generation", bin, fixture_msg_scalar_6);
-
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_7,
-				      sizeof(fixture_m_7),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 7");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 7 generation", bin, fixture_msg_scalar_7);
-
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_8,
-				      sizeof(fixture_m_8),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 8");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 8 generation", bin, fixture_msg_scalar_8);
-
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_9,
-				      sizeof(fixture_m_9),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 9");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 9 generation", bin, fixture_msg_scalar_9);
-
-	if (BBS_OK != hash_to_scalar (&cipher_suite,
-				      scalar,
-				      map_dst,
-				      map_dst_len,
-				      fixture_m_10,
-				      sizeof(fixture_m_10),
-				      0))
-	{
-		puts ("Error during hash to scalar for message 10");
-		return 1;
-	}
-	RLC_TRY {
-		bn_write_bbs (bin, scalar);
-	} RLC_CATCH_ANY { puts ("Internal Error"); return 1; }
-	ASSERT_EQ ("scalar 10 generation", bin, fixture_msg_scalar_10);
 
 	bn_free (scalar);
 	return 0;
