1dab40fc024566a80cc359df219353eaa5ba21b7
Stephan Schmiedmayer <56692976+StephanSchmiedmayer@users.noreply.github.com>
2024-04-30 16:52:53 +0200

    Add benchmark results to readme

--- README.md
+++ README.md
@@ -43,3 +43,17 @@ Within `build`:
 ```zsh
 make bench
 ```
+
+Benchmark on Apple M1 Pro 2021 16GB:
+
+- 2 messages each of size 64 bytes
+- Disclosing first message only
+- Runtime averaged over 1000 iterations
+
+| Function                 | SHA256 (ms) | SHAKE256 (ms) |
+| ------------------------ | ----------- | ------------- |
+| key generation (SK & PK) | 0,174       | 0,172         |
+| sign                     | 1,995       | 1,888         |
+| verify                   | 3,877       | 3,829         |
+| proof generation         | 3,289       | 3,220         |
+| proof verification       | 4,532       | 4,482         |
--- test/bbs_bench_individual.c
+++ test/bbs_bench_individual.c
@@ -3,22 +3,22 @@
 #include <string.h>
 
 #define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
+#define TOSTRING(x)  STRINGIFY (x)
 
 int
 bbs_bench_individual ()
 {
-	#define CIPHERSUITE sha256
+	#define CIPHERSUITE                         sha256
 	// Use an additional macro to ensure full expansion before concatenation
-	#define CONCAT_INTERNAL(a, b, c) a##_##b##_##c
-	#define BBS_EXECUTE(cipher_suite, function) CONCAT_INTERNAL(bbs, cipher_suite, function)
+	#define CONCAT_INTERNAL(a, b, c)            a ## _ ## b ## _ ## c
+	#define BBS_EXECUTE(cipher_suite, function) CONCAT_INTERNAL (bbs, cipher_suite, function)
 
-	#define USE_HEADER 0
+	#define USE_HEADER                          0
 
-	printf("Benchmarking %s \n", TOSTRING(CIPHERSUITE));
+	printf ("Benchmarking %s \n",    TOSTRING (CIPHERSUITE));
 	printf ("Include header: %d \n", USE_HEADER);
 
-	#define ITERATIONS 100
+	#define ITERATIONS 1000
 
 	if (core_init () != RLC_OK)
 	{
@@ -35,12 +35,12 @@ bbs_bench_individual ()
 	bbs_secret_key sk[ITERATIONS];
 	bbs_public_key pk[ITERATIONS];
 
-	printf ("%s key generation %d iterations.\n", TOSTRING(CIPHERSUITE), ITERATIONS);
+	printf ("%s key generation %d iterations.\n", TOSTRING (CIPHERSUITE), ITERATIONS);
 
 	BBS_BENCH_START (key_gen)
 	for (int i = 0; i < ITERATIONS; i++)
 	{
-		if (BBS_OK != BBS_EXECUTE(CIPHERSUITE, keygen_full) (sk[i], pk[i]))
+		if (BBS_OK != BBS_EXECUTE (CIPHERSUITE, keygen_full) (sk[i], pk[i]))
 		{
 			puts ("Error during key generation");
 			return 1;
@@ -50,7 +50,7 @@ bbs_bench_individual ()
 
 	// - MARK: Signing
 
-	#define MSG_LEN    64
+	#define MSG_LEN 64
 
 	char msg1[ITERATIONS][MSG_LEN];
 	char          msg2[ITERATIONS][MSG_LEN];
@@ -71,15 +71,15 @@ bbs_bench_individual ()
 	#endif
 
 	printf ("%s signing %d iterations of %d messages each of size %d bytes.\n",
-					TOSTRING(CIPHERSUITE), ITERATIONS,
-		2,
-		MSG_LEN);
+		TOSTRING (CIPHERSUITE), ITERATIONS, 2, MSG_LEN);
 
 	BBS_BENCH_START (sign)
 	for (int i = 0; i < ITERATIONS; i++)
 	{
-		if (BBS_OK != BBS_EXECUTE(CIPHERSUITE, sign) (sk[i], pk[i], sig[i], (uint8_t*) header, strlen (header), 2,
-					       msg1[i], MSG_LEN, msg2[i], MSG_LEN))
+		if (BBS_OK != BBS_EXECUTE (CIPHERSUITE, sign) (sk[i], pk[i], sig[i],
+							       (uint8_t*) header, strlen (header),
+							       2, msg1[i], MSG_LEN, msg2[i],
+							       MSG_LEN))
 		{
 			puts ("Error during signing");
 			return 1;
@@ -89,14 +89,12 @@ bbs_bench_individual ()
 
 	// - MARK: Verification
 	printf ("%s verification %d iterations of %d messages each of size %d bytes.\n",
-					 TOSTRING(CIPHERSUITE), ITERATIONS,
-		2,
-		MSG_LEN);
+		TOSTRING (CIPHERSUITE), ITERATIONS, 2, MSG_LEN);
 	BBS_BENCH_START (verify)
 	for (int i = 0; i < ITERATIONS; i++)
 	{
-		if (BBS_OK != bbs_sha256_verify (pk[i], sig[i], (uint8_t*) header, strlen (header), 2,
-						 msg1[i], MSG_LEN, msg2[i], MSG_LEN))
+		if (BBS_OK != BBS_EXECUTE (CIPHERSUITE, verify) (pk[i], sig[i], (uint8_t*) header, strlen (header),
+						 2, msg1[i], MSG_LEN, msg2[i], MSG_LEN))
 		{
 			puts ("Error during signature verification");
 			return 1;
@@ -105,8 +103,8 @@ bbs_bench_individual ()
 	BBS_BENCH_END (verify, "Verification")
 
 	// - MARK: Proof generation
-	uint8_t     proof[ITERATIONS][BBS_PROOF_LEN (1)];
-	uint64_t    disclosed_indexes[] = {0};
+	uint8_t proof[ITERATIONS][BBS_PROOF_LEN (1)];
+	uint64_t       disclosed_indexes[] = {0};
 	#define RANDOM_NONCE_SIZE 23
 	static uint8_t random_nonces[ITERATIONS][RANDOM_NONCE_SIZE];
 	for (int i = 0; i < ITERATIONS; i++)
@@ -117,27 +115,23 @@ bbs_bench_individual ()
 		}
 	}
 
-	printf ("%s proof generation %d iterations of %d messages each of size %d bytes disclosing first message only.\n",
-					 TOSTRING(CIPHERSUITE), ITERATIONS,
+	printf (
+		"%s proof generation %d iterations of %d messages each of size %d bytes disclosing first message only.\n",
+		TOSTRING (CIPHERSUITE),
+		ITERATIONS,
 		2,
 		MSG_LEN);
 	BBS_BENCH_START (proof_gen)
 	for (int i = 0; i < ITERATIONS; i++)
 	{
-		if (BBS_OK != BBS_EXECUTE(CIPHERSUITE, proof_gen) (pk[i],
-					    sig[i],
-					    proof[i],
-					    (uint8_t*) header,
-					    strlen (header),
-					    (uint8_t*) random_nonces[i],
-					    RANDOM_NONCE_SIZE,
-					    disclosed_indexes,
-					    1,
-					    2,
-					    msg1[i],
-					    MSG_LEN,
-					    msg2[i],
-					    MSG_LEN))
+		if (BBS_OK != BBS_EXECUTE (CIPHERSUITE, proof_gen) (pk[i], sig[i], proof[i],
+								    (uint8_t*) header,
+								    strlen (header),
+								    (uint8_t*) random_nonces[i],
+								    RANDOM_NONCE_SIZE,
+								    disclosed_indexes, 1, 2,
+								    msg1[i], MSG_LEN, msg2[i],
+								    MSG_LEN))
 		{
 			puts ("Error during proof generation");
 			return 1;
@@ -146,25 +140,20 @@ bbs_bench_individual ()
 	BBS_BENCH_END (proof_gen, "Proof generation")
 
 	// - MARK: Proof verification
-	printf ("%s proof verification %d iterations of %d messages each of size %d bytes disclosing first message only.\n",
-					 TOSTRING(CIPHERSUITE), ITERATIONS,
+	printf (
+		"%s proof verification %d iterations of %d messages each of size %d bytes disclosing first message only.\n",
+		TOSTRING (CIPHERSUITE),
+		ITERATIONS,
 		2,
 		MSG_LEN);
 	BBS_BENCH_START (proof_verify)
 	for (int i = 0; i < ITERATIONS; i++)
 	{
-		if (BBS_OK != bbs_sha256_proof_verify (pk[i],
-					       proof[i],
-					       BBS_PROOF_LEN (1),
-					       (uint8_t*) header,
-					       strlen (header),
-					       (uint8_t*) random_nonces[i],
-					       RANDOM_NONCE_SIZE,
-					       disclosed_indexes,
-					       1,
-					       2,
-					       msg1[i],
-					       MSG_LEN))
+		if (BBS_OK != BBS_EXECUTE(CIPHERSUITE, proof_verify) (pk[i], proof[i], BBS_PROOF_LEN (1),
+						       (uint8_t*) header, strlen (header),
+						       (uint8_t*) random_nonces[i],
+						       RANDOM_NONCE_SIZE, disclosed_indexes, 1, 2,
+						       msg1[i], MSG_LEN))
 		{
 			puts ("Error during proof verification");
 			return 1;
