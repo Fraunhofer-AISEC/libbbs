c3e3aa07fbd7592b6bf9f0c75f0d80bfe29317db
Martin Schanzenbach <schanzen@gnunet.org>
2025-07-14 08:56:56 +0200

    portability fixes

--- src/sha256.h
+++ src/sha256.h
@@ -5,9 +5,11 @@
 #include <stdint.h>
 #include <string.h>
 
-#ifdef __GLIBC__
+#if defined(__GLIBC__)
 // Dear glibc team. Please support C23. Thank you.
 #define memset_explicit(s, c, n) explicit_bzero(s,n);
+#elif defined(__APPLE__)
+#define memset_explicit(s, c, n) memset_s(s, n, c, n);
 #endif
 
 /* SHA256 */
